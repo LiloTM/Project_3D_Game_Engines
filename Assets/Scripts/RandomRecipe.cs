ec3a95cbfa16b2124a426e95ba70db999aa5cd4f
Snowlilo <Laura-U@kabelmail.de>
2022-01-16 01:47:07 +0100

    added functioning recipes

--- Assets/Scripts/RandomRecipe.cs
+++ Assets/Scripts/RandomRecipe.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class RandomRecipe : MonoBehaviour
 {
     int random;
+    int RecipeNumber = 0;
     float counter;
     public float timeRemaining = 100;
+    public Transform Kaffee;
+    public Transform Pizza;
+    public List<Transform> allRecipes;
+    public Transform UI;
 
     void Start()
     {
@@ -32,6 +37,7 @@ public class RandomRecipe : MonoBehaviour
                 case 1: drawCoffee(); break;
                 case 2: drawPizza(); break;
             }
+            RecipeNumber++;
         }
     }
 
@@ -40,12 +46,41 @@ public class RandomRecipe : MonoBehaviour
         if(random<50) {random = 1;} else {random = 2;}
     }
 
+//TODO: REFACTOR THIS LILO!!
     void drawCoffee(){
-
+        Transform newCoffee = Instantiate (Kaffee, UI);     
+        newCoffee.transform.position = newCoffee.position + new Vector3(RecipeNumber*180,0,0);
+        allRecipes.Add(newCoffee);
     }
 
     void drawPizza(){
-        
+        Transform newPizza = Instantiate (Pizza, UI);
+        newPizza.transform.position = newPizza.position + new Vector3(RecipeNumber*180,0,0);
+        allRecipes.Add(newPizza);
+    }
+
+    public void destroyCoffeeRecipe() {
+        var checker = false;
+        foreach(Transform item in allRecipes){
+            if(item.CompareTag("CoffeeRecipe") && checker==false){
+                Destroy(item.gameObject);
+                checker = true;
+            }
+            if(checker == true) item.transform.position = item.position - new Vector3(180,0,0); 
+        }
+        RecipeNumber--;
+    }
+
+    public void destroyPizzaRecipe() {
+        var checker = false;
+        foreach(Transform item in allRecipes){
+            if(item.CompareTag("PizzaRecipe") && checker==false){
+                Destroy(item.gameObject);
+                checker = true;
+            }
+            if(checker == true) item.transform.position = item.position - new Vector3(180,0,0);
+        }
+        RecipeNumber--;
     }
 
 }
